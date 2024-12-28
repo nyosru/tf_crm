b56908f2821621d4c61b8ae139d908ea38f167e8
Sergey <1@php-cat.com>
2024-12-29 04:14:10 +0500

    12

--- database/migrations/2024_12_19_142555_change_date_passport_field_in_clients_table.php
+++ database/migrations/2024_12_19_142555_change_date_passport_field_in_clients_table.php
@@ -13,17 +13,22 @@ return new class extends Migration
     public function up(): void
     {
 
-        //
-
-        // Сначала удаляем все записи с датой '0000-00-00'
-        DB::table('clients')->where('date_passport', '=','0000-00-00')->update(['date_passport' => '1900-01-01']);
-
         Schema::table('clients', function (Blueprint $table) {
-            $table->date('date_passport')->nullable(true)->change();
+            // Изменяем тип поля на дату с возможностью хранения NULL
+            $table->date('date_passport')->nullable()->change();
         });
 
-        DB::table('clients')->where('date_passport', '1900-01-01')->update(['date_passport' => null]);
-//        DB::table('clients')->where('date_passport', '0000-00-00')->update(['date_passport' => null]);
+//        //
+//
+//        // Сначала удаляем все записи с датой '0000-00-00'
+//        DB::table('clients')->where('date_passport', '=','0000-00-00')->update(['date_passport' => '1900-01-01']);
+//
+//        Schema::table('clients', function (Blueprint $table) {
+//            $table->date('date_passport')->nullable(true)->change();
+//        });
+//
+//        DB::table('clients')->where('date_passport', '1900-01-01')->update(['date_passport' => null]);
+////        DB::table('clients')->where('date_passport', '0000-00-00')->update(['date_passport' => null]);
     }
 
     /**
