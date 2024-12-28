c2bc52536e395385e864ee35a4d210878a92be98
Sergey <1@php-cat.com>
2024-12-29 03:45:08 +0500

    бд

--- database/migrations/2024_12_19_142555_change_date_passport_field_in_clients_table.php
+++ database/migrations/2024_12_19_142555_change_date_passport_field_in_clients_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     public function up(): void
     {
         // Сначала удаляем все записи с датой '0000-00-00'
-//        DB::table('clients')->where('date_passport', '0000-00-00')->update(['date_passport' => '1900-01-01']);
+        DB::table('clients')->where('date_passport', '0000-00-00')->update(['date_passport' => '1900-01-01']);
 
         Schema::table('clients', function (Blueprint $table) {
             $table->date('date_passport')->nullable(true)->change();
@@ -21,7 +21,7 @@ return new class extends Migration
 
 //        DB::table('clients')->where('date_passport', '0000-00-00')->update(['date_passport' => '1900-01-01']);
         DB::table('clients')->where('date_passport', '0000-00-00')->update(['date_passport' => null]);
-        
+
     }
 
     /**
